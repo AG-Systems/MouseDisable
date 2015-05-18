92acce74f5c71f124428085efb330c04ea88ad8f
MeepoKid <Empathy@smarttechadvice.com>
2015-05-18 14:48:13 -0700

    Final

---
+++ MouseDisable/MouseDisable/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseDisable
+{
+    public partial class MouseDisable : Form
+    {
+        public MouseDisable()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        Point dPosition;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dPosition.X = 0;
+            dPosition.Y = 0;
+            Cursor.Position = dPosition;
+        }
+    }
+}
